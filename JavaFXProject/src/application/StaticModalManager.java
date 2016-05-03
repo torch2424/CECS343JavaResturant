9acadb3c1eaa6dc9e6ef1c92a0717f0e4a3ab947
Aaron Turner <aaron@aaronthedev.com>
2016-05-02 20:28:23 -0700

    Finished up the object orientated modal display

--- JavaFXProject/src/application/StaticModalManager.java
+++ JavaFXProject/src/application/StaticModalManager.java
@@ -16,35 +16,32 @@ public class StaticModalManager {
 
 	public static void tableModal(FXMLLoader loader, ActionEvent event) {
 
-		Stage stage = new Stage();
-	    Parent root;
+		//Get our controller
+	    TablePopupControl controller = loader.<TablePopupControl>getController();
 
-	    try {
+	    //Call controller Function
+	    //controller.initData(customer);
 
-			//Get our FXML Loader
-			root = loader.load();
-
-		    stage.setScene(new Scene(root));
-		    stage.setTitle("Tables");
-		    stage.initModality(Modality.WINDOW_MODAL);
-		    stage.initOwner(
-		        ((Node)event.getSource()).getScene().getWindow() );
-
-		    //Get our controller
-		    TablePopupControl controller = loader.<TablePopupControl>getController();
-
-		    //Call controller Function
-		    //controller.initData(customer);
-
-		    	  //Finally show the stage
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	    //Pass to the modal Creater
+	    StaticModalManager.createModal(loader, event, "Seats");
 	}
 
 	public static void seatModal(FXMLLoader loader, ActionEvent event) {
 
+
+	    //Get our controller
+	    SeatPopupControl controller = loader.<SeatPopupControl>getController();
+
+	    //Call controller Function
+	    //controller.initData(customer);
+
+	    //Pass to the modal Creater
+	    StaticModalManager.createModal(loader, event, "Seats");
+	}
+
+	//Helper Fucntion to create the modal
+	private static void createModal(FXMLLoader loader, ActionEvent event, String title) {
+
 		Stage stage = new Stage();
 	    Parent root;
 
@@ -54,21 +51,35 @@ public class StaticModalManager {
 			root = loader.load();
 
 		    stage.setScene(new Scene(root));
-		    stage.setTitle("Seats");
+		    stage.setTitle(title);
 		    stage.initModality(Modality.WINDOW_MODAL);
 		    stage.initOwner(
 		        ((Node)event.getSource()).getScene().getWindow() );
 
-		    //Get our controller
-		    SeatPopupControl controller = loader.<SeatPopupControl>getController();
-
-		    //Call controller Function
-		    //controller.initData(customer);
-
 		    //Finally show the stage
+		    stage.show();
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+
+			StaticModalManager.modalError();
 		}
+	}
+
+
+	//Helper function to easily close modals
+	public static void closeModal(ActionEvent event) {
+
+		//Simply close the modal
+		Node source = (Node) event.getSource();
+		Stage stage = (Stage) source.getScene().getWindow();
+		stage.close();
+	}
+
+	//Function to catch modal Errors
+	public static void modalError() {
+
+		System.out.println("JavaFX had an error");
 	}
 }
