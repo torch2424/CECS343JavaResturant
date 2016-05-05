0f02965d9b56faaa884a914285811a1769088281
Rich Cherngchaosil <richer_po@hotmail.com>
2016-05-04 22:03:15 -0700

    Add Table class for database, created addTable function

--- JavaFXProject/src/application/ConnectFirebase.java
+++ JavaFXProject/src/application/ConnectFirebase.java
@@ -2,10 +2,51 @@ package application;
 
 import com.firebase.client.Firebase;
 
+/**
+ *
+ * @author Rich Cherngchaosil
+ *
+ * This class connects to the Firebase server and will handle the get and update to the server
+ *
+ */
 public class ConnectFirebase {
 
-	Firebase ref = new Firebase("https://docs-examples.firebaseio.com/android/saving-data/fireblog");
+	Firebase root;
+	Thread thread;
+
 	public ConnectFirebase(){
+		root = new Firebase("https://cecs343javaproject.firebaseio.com");
+	}
+
+	public Firebase addTable(){
+		Firebase tableRef = root.child("Table");
+
+		Table table = new Table();
+		Firebase newTableRef = tableRef.push();
+		newTableRef.setValue(table);
+
+		thread = new Thread(){
+			public void run(){
+				try {
+		            System.out.println("Creating Table....");
+		            Thread.sleep(1000);
+		        } catch(InterruptedException v) {
+		            System.out.println(v);
+		        }
+			}
+		};
+		thread.start();
+
+		return newTableRef;
+	}
+
+	public static void main(String[] args){
+		ConnectFirebase f = new ConnectFirebase();
+		System.out.println(f.addTable());
+		System.out.println("check");
+
 
 	}
+
+
 }
---
+++ JavaFXProject/src/application/Table.java
@@ -0,0 +1,39 @@
+package application;
+
+/**
+ *
+ * @author Rich Cherngchaosil
+ *
+ * Object to store in the database
+ *
+ */
+public class Table {
+
+	/**
+	 * state:
+	 * 	0 - Ready
+	 * 	1 - Occ'd
+	 *  2 - Dirty
+	 */
+	private int state;
+
+	public Table(){
+		state = 2;
+	}
+
+	public int getTableState(){
+		return state;
+	}
+
+	public void updateTableState(int s){
+		if(s < 0 || s > 2){
+			System.out.println("Unknown State, cannot update the state of the table");
+		}
+
+		state = s;
+	}
+
+	public void clearTable(){
+
+	}
+}
