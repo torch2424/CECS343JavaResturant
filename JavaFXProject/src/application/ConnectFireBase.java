dba3a690ecb73b5d50ce1bd2696f43a6be7d03bb
Rich Cherngchaosil <richer_po@hotmail.com>
2016-05-04 21:13:39 -0700

    Added FireBase Library

---
+++ JavaFXProject/src/application/ConnectFireBase.java
@@ -0,0 +1,8 @@
+package application;
+
+import com.firebase.client.Firebase;
+
+public class ConnectFireBase {
+
+	Firebase ref = new Firebase("https://docs-examples.firebaseio.com/android/saving-data/fireblog");
+}
