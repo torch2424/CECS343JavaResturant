58059602f7ea6b1a16b6192260172debc4ed9c18
Rich Cherngchaosil <richer_po@hotmail.com>
2016-05-04 21:16:18 -0700

    Rename

--- JavaFXProject/src/application/ConnectFirebase.java
+++ JavaFXProject/src/application/ConnectFirebase.java
@@ -2,7 +2,10 @@ package application;
 
 import com.firebase.client.Firebase;
 
-public class ConnectFireBase {
+public class ConnectFirebase {
 
 	Firebase ref = new Firebase("https://docs-examples.firebaseio.com/android/saving-data/fireblog");
+	public ConnectFirebase(){
+
+	}
 }
