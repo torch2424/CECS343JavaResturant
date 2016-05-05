c9c63816a38ed8ccaa4aa5dc87c7bd281a84d39d
Aaron Turner <aaron@aaronthedev.com>
2016-05-04 20:22:01 -0700

    Merged projects into one

--- JavaFXProject/src/application/GuiController.java
+++ JavaFXProject/src/application/GuiController.java
@@ -8,14 +8,47 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ListView;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import modalPopups.createOrEditControl;
 import modalPopups.createOrEditControl.ResturantObject;
 
 public class GuiController {
+
+	public Scene mainScene;
+	public void setMainScene(Scene pScene){
+		mainScene = pScene;
+	}
+
+	@FXML
+	private Rectangle tableOneRectangle;
+	@FXML
+	private SplitMenuButton menuButtonOne;
+	@FXML
+	private TextField newItemTextField;
+	@FXML
+	private Button newItemSaveButton;
+	@FXML
+	private Rectangle tableTwoRectangle;
+	@FXML
+	private SplitMenuButton menuButtonTwo;
+	@FXML
+	private Rectangle tableThreeRectangle;
+	@FXML
+	private Rectangle tableFourRectangle;
+	@FXML
+	private SplitMenuButton currentMenuButton;
+	@FXML
+	private SplitMenuButton menuButtonThree;
+	@FXML
+	private SplitMenuButton menuButtonFour;
 
 	//Our table view from scene builder
 	@FXML
@@ -75,6 +108,96 @@ public class GuiController {
 
 			StaticModalManager.modalError();
 		}
+	}
+
+	public void readyButtonTableOneClicked() {
+		tableOneRectangle.setFill(Color.GREEN);
+	}
+	public void occupyButtonTableOneClicked() {
+		tableOneRectangle.setFill(Color.RED);
+	}
+	public void dirtyButtonTableOneClick() {
+		tableOneRectangle.setFill(Color.YELLOW);
+	}
+
+
+	public void addMenuOne() {
+		currentMenuButton = menuButtonOne;
+		newItemTextField.setDisable(false);
+		newItemSaveButton.setDisable(false);
+	}
+	public void addMenuTwo() {
+		currentMenuButton = menuButtonTwo;
+		newItemTextField.setDisable(false);
+		newItemSaveButton.setDisable(false);
+	}
+	public void addMenuThree() {
+		currentMenuButton = menuButtonThree;
+		newItemTextField.setDisable(false);
+		newItemSaveButton.setDisable(false);
+	}
+	public void addMenuFour() {
+		currentMenuButton = menuButtonFour;
+		newItemTextField.setDisable(false);
+		newItemSaveButton.setDisable(false);
+	}
+	public void saveNewItem() {
+		MenuItem newMenuItem = new MenuItem(newItemTextField.getText());
+		newMenuItem.setOnAction(e -> {
+			newMenuItem.setText(newMenuItem.getText() + " - SERVED");
+		});
+		currentMenuButton.getItems().addAll(newMenuItem);
+		newItemTextField.clear();
+		newItemTextField.setPromptText("New item...");
+		newItemTextField.setDisable(true);
+		newItemSaveButton.setDisable(true);
+	}
+	public void clearTable() {
+		tableOneRectangle.setFill(Color.GREEN);
+		menuButtonOne.getItems().clear();
+	}
+	public void clearTableTwo() {
+		tableTwoRectangle.setFill(Color.GREEN);
+		menuButtonTwo.getItems().clear();
+	}
+	public void clearTableThree() {
+		tableThreeRectangle.setFill(Color.GREEN);
+		menuButtonThree.getItems().clear();
+	}
+	public void clearTableFour() {
+		tableFourRectangle.setFill(Color.GREEN);
+		menuButtonFour.getItems().clear();
+	}
+
+
+	public void readyButtonTableTwoClicked() {
+		tableTwoRectangle.setFill(Color.GREEN);
+	}
+	public void occupyButtonTableTwoClicked() {
+		tableTwoRectangle.setFill(Color.RED);
+	}
+	public void dirtyButtonTableTwoClick() {
+		tableTwoRectangle.setFill(Color.YELLOW);
+	}
+
+	public void readyButtonTableThreeClicked() {
+		tableThreeRectangle.setFill(Color.GREEN);
+	}
+	public void occupyButtonTableThreeClicked() {
+		tableThreeRectangle.setFill(Color.RED);
+	}
+	public void dirtyButtonTableThreeClick() {
+		tableThreeRectangle.setFill(Color.YELLOW);
+	}
+
+	public void readyButtonTableFourClicked() {
+		tableFourRectangle.setFill(Color.GREEN);
+	}
+	public void occupyButtonTableFourClicked() {
+		tableFourRectangle.setFill(Color.RED);
+	}
+	public void dirtyButtonTableFourClick() {
+		tableFourRectangle.setFill(Color.YELLOW);
 	}
 
 
