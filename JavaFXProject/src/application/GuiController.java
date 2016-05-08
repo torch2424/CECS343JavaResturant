e49a68719aba1a884e409ee24a5ab032d58c406b
Aaron Turner <aaron@aaronthedev.com>
2016-05-07 18:41:13 -0700

    Removed all Uneccessary Code
    As the title states
    
    Also, fixed a slight bug in the analysis

--- JavaFXProject/src/application/GuiController.java
+++ JavaFXProject/src/application/GuiController.java
@@ -7,16 +7,10 @@ import guiElements.TableController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-import modalPopups.createOrEditControl;
-import modalPopups.createOrEditControl.ResturantObject;
 import rAnalysis.RAnalysis;
 import rModels.RItem;
 
@@ -71,7 +65,7 @@ public class GuiController {
 		int newTableIndex =  TasteMain.getTables().size();
 
 		//Add it to our main Controllers
-		TasteMain.addTable("Table" + (newTableIndex + 1), 6);
+		TasteMain.addTable("Table" + (newTableIndex + 1));
 
 		//Edit the table status
 	    tableControllers.add(loader.<TableController>getController());
@@ -126,61 +120,5 @@ public class GuiController {
 
 		//Call the close modal static
 		System.exit(0);
-	}
-
-	//Table Editor
-	@FXML
-	private void tableManage(ActionEvent event) {
-
-		//Call the helper function
-		createOrEdit(event, ResturantObject.TABLE);
-	}
-
-	//Seat Editor
-	@FXML
-	private void seatManage(ActionEvent event) {
-
-		//Call the helper function
-		createOrEdit(event, ResturantObject.SEAT);
-	}
-
-	//Helper Function to open modal to select from a list of created objects to edit/or create a new one
-	private void createOrEdit(ActionEvent event, ResturantObject objectType) {
-
-		Stage stage = new Stage();
-	    Parent root;
-		try {
-
-			//Get our FXML Loader
-			FXMLLoader loader = new FXMLLoader(
-				    getClass().getResource("../modalPopups/createOrEdit.fxml"));
-
-			root = loader.load();
-
-		    stage.setScene(new Scene(root));
-		    stage.setTitle("Resturant Edit - " + objectType.toString());
-		    stage.initModality(Modality.WINDOW_MODAL);
-		    stage.initOwner(
-		        ((Node)event.getSource()).getScene().getWindow() );
-
-		    //Get our controller
-		    createOrEditControl controller = loader.<createOrEditControl>getController();
-
-		    //Call controller Function
-		    if(objectType == ResturantObject.TABLE)
-		    	controller.initController(createOrEditControl.ResturantObject.TABLE);
-		    else if(objectType == ResturantObject.SEAT)
-		    	controller.initController(createOrEditControl.ResturantObject.SEAT);
-
-		    //Finally show the stage
-		    stage.show();
-
-		} catch (IOException e) {
-
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-
-			StaticModalManager.modalError();
-		}
 	}
 }
